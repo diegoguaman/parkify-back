721980447f34afc28521d9c2fd27f41f0d34b5a8
zapisator <zapisator@logan-club.ru>
2025-04-03 10:37:07 -0300

    fix(models): Add minimum properties (id field, annotations) to start the project

--- src/main/java/com/igrowker/feature/parkify/models/ParkingSpot.java
+++ src/main/java/com/igrowker/feature/parkify/models/ParkingSpot.java
@@ -1,6 +1,9 @@
 package com.igrowker.feature.parkify.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +15,7 @@ import lombok.Setter;
 @Entity
 @Table(name="parkingSpot")
 public class ParkingSpot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
