f8597dc1a1748188bad635a3a1031f1c03b6a450
Krisel <kriselur@gmail.com>
2025-04-19 14:49:48 +0200

    Code update and verification of functional endpoints

--- src/main/java/com/igrowker/feature/parkify/features/auth/dto/request/RegisterRequest.java
+++ src/main/java/com/igrowker/feature/parkify/features/auth/dto/request/RegisterRequest.java
@@ -6,21 +6,21 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
-        @NotBlank(message = "Username cannot be blank")
+//        @NotBlank(message = "Username cannot be blank")
         String username,
 
         @NotBlank(message = "Email cannot be blank")
         @Email(message = "Invalid email format")
         String email,
 
-        @NotBlank(message = "Password cannot be blank")
+//        @NotBlank(message = "Password cannot be blank")
         @Size(min = 6, message = "Password must be at least 6 characters long")
         String password,
 
-        @NotBlank(message = "Role cannot be blank")
+//        @NotBlank(message = "Role cannot be blank")
         @Pattern(regexp = "^(DRIVER|OWNER)$", message = "Role must be either DRIVER or OWNER")
         String role,
 
-        @NotBlank(message = "Contact phone cannot be blank")
+//        @NotBlank(message = "Contact phone cannot be blank")
         String contactPhone) {
 }
