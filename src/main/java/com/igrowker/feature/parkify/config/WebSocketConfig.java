8f12ab31e83238c881c26811300fd39536aa3f94
maurifigueroa <mauricio.david.figueroa@gmail.com>
2025-04-23 03:49:30 -0300

    Se modifica web config

--- src/main/java/com/igrowker/feature/parkify/config/WebSocketConfig.java
+++ src/main/java/com/igrowker/feature/parkify/config/WebSocketConfig.java
@@ -5,6 +5,8 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import com.igrowker.feature.parkify.websocket.YourWebSocketHandler;
+
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
